b9465ed0150cd9272fd35dd0e89303d0482eb648
Celinaki <celina1999@live.se>
2024-05-21 23:19:49 +0200

    Mobile friendly base style for single recipe page implemented

--- client/src/components/SingleRecipe.tsx
+++ client/src/components/SingleRecipe.tsx
@@ -1,76 +1,136 @@
-import style from '../cssmodules/SingleRecipeMod.module.css'
-import Sidebar from './Sidebar'
-import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import StarRatings from './ratings'
-import Navbar from './Navbar'
+import style from "../cssmodules/SingleRecipeMod.module.css";
+import Sidebar from "./Sidebar";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import StarRatings from "./ratings";
+import Navbar from "./Navbar";
+import { FaStar, FaRegClock } from "react-icons/fa";
 
 const SingleRecipe = () => {
-    const [recipe, setRecipe] = useState<any>({});
-    const { recipeId } = useParams()
-    useEffect(() => {
-        console.log(recipeId)
-        const url = `https://planned-sammy-celinaki.koyeb.app/recipes/${recipeId}`
-        console.log(url)
-        axios.get(url)
-            .then(res => {
-                console.log(res)
-                setRecipe(res.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, []);
-    const [voted, setVote] = useState(false)
-    const setClicked = () => {
-        setVote(true)
+  const [recipe, setRecipe] = useState<any>({});
+  const { recipeId } = useParams();
+  useEffect(() => {
+    console.log(recipeId);
+    const url = `https://planned-sammy-celinaki.koyeb.app/recipes/${recipeId}`;
+    console.log(url);
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res);
+        setRecipe(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const [voted, setVote] = useState(false);
+  const setClicked = () => {
+    setVote(true);
+  };
+  const calculateAverage = (rating: any[]): number => {
+    const middleValue = rating?.reduce((a: number, b: number) => a + b);
+    const average = middleValue / rating?.length;
+    return parseFloat(average.toFixed(1));
+  };
 
+  return (
+    <>
+      {/* <Navbar /> */}
+      <div className={style.bigwrapper}>
+        {/* <Sidebar /> */}
+        <div className={style.recipewrap}>
+          <article className={style.card}>
+            {/* <h1>{recipe.title}</h1> */}
+            <section className={style.imgdescr}>
+              <img src={recipe.imgUrl} alt="" className={style.bigimg} />
+              {/* <h2>{recipe.description} </h2> */}
+            </section>
+            <div className={style.content}>
+              <h1>{recipe.title}</h1>
+              <div
+                style={{ display: "flex", gap: "2rem", marginBottom: "2rem" }}
+              >
+                <span style={{ alignItems: "center", display: "flex" }}>
+                  <FaStar
+                    title={"Rating stars"}
+                    size={26}
+                    color={"rgb(241, 210, 32)"}
+                  />
+                  <span
+                    style={{
+                      marginLeft: "0.8rem",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    {recipe.ratings && calculateAverage(recipe.ratings)}
+                  </span>
+                </span>
+                <span style={{ display: "flex", alignItems: "center" }}>
+                  <FaRegClock title={"Time"} size={26} />
+                  <span
+                    style={{
+                      marginLeft: "0.8rem",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    10min
+                  </span>
+                </span>
+              </div>
 
-    }
-
-    return (
-        <>
-
-            <Navbar />
-            <div className={style.bigwrapper}>
-                <Sidebar />
-                <div className={style.recipewrap}>
-                    <article className={style.card}>
-                        <h1>{recipe.title}</h1>
-                        <section className={style.imgdescr}>
-                            <img src={recipe.imgUrl} alt="" className={style.bigimg} />
-                            <h2>{recipe.description} </h2>
-                        </section>
-                        <section className={style.instructions}>
-                            <section className={style.lists}>
-                                <h2>Ingredienser</h2>
-                                <ul className={style.nodec}>
-                                    {recipe.ingredients && recipe.ingredients?.map((ingredient: any) => (
-                                        <li className={style.vegan} key={recipe._id}>&#x1f331;{ingredient.name.charAt(0).toUpperCase() + ingredient.name.slice(1)} {ingredient.amount} {ingredient.unit}</li>
-                                    ))}                        </ul>
-                            </section>
-                            <section className={style.lists}>
-                                <h2>Instruktioner</h2>
-                                <ol className={style.ul}>
-                                    {recipe.instructions && recipe.instructions?.map((step: any) => (
-                                        <li key={step.index}>{step}</li>
-                                    ))}
-                                </ol>
-                            </section>
-                        </section>
-                    </article>
-                    <section className={style.rating}>
-
-                        {/* <span  {setClicked((clicked) => false &&)} > test</span> */}
-                        {(voted === false) ? <><h1>Lägg ett betyg!</h1> <span onClick={setClicked}>{recipe.ratings && <StarRatings edit={true} recipeId={recipe._id} recipeRatings={recipe.ratings} />}</span> </> : <h1>Tack för ditt betyg!</h1>}
-                    </section>
+              <div className={style.instructions}>
+                <div className={style.lists}>
+                  <h2>Ingredients</h2>
+                  <ul className={style.plantList}>
+                    {recipe.ingredients &&
+                      recipe.ingredients?.map((ingredient: any) => (
+                        <li
+                          className={style.vegan}
+                          key={`${ingredient.index}_ingredient`}
+                        >
+                          &#x1f331;
+                          {ingredient.name.charAt(0).toUpperCase() +
+                            ingredient.name.slice(1)}{" "}
+                          {ingredient.amount} {ingredient.unit}
+                        </li>
+                      ))}{" "}
+                  </ul>
                 </div>
-
-
+                <div className={style.lists}>
+                  <h2>Instructions</h2>
+                  <ol className={style.ul}>
+                    {recipe.instructions &&
+                      recipe.instructions?.map((step: any) => (
+                        <li key={step.index}>{step}</li>
+                      ))}
+                  </ol>
+                </div>
+              </div>
+              <section className={style.rating}>
+                {/* <span  {setClicked((clicked) => false &&)} > test</span> */}
+                {voted === false ? (
+                  <>
+                    <h2>What did you think?</h2>{" "}
+                    <span onClick={setClicked}>
+                      {recipe.ratings && (
+                        <StarRatings
+                          edit={true}
+                          recipeId={recipe._id}
+                          recipeRatings={recipe.ratings}
+                        />
+                      )}
+                    </span>{" "}
+                  </>
+                ) : (
+                  <h1>Your rating has been added!</h1>
+                )}
+              </section>
             </div>
-        </>
-
-    )
-}
-export default SingleRecipe
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+export default SingleRecipe;
--- client/src/components/ratings.tsx
+++ client/src/components/ratings.tsx
@@ -1,49 +1,45 @@
-import styled from 'styled-components'
-import ReactStars from 'react-stars'
-import React from 'react'
-import {postRating} from '../api/index'
-
+import styled from "styled-components";
+import ReactStars from "react-stars";
+import React from "react";
+import { postRating } from "../api/index";
 
 interface ratingProps {
-    recipeRatings: Array<number>
-    recipeId: string
-    edit: boolean
+  recipeRatings: Array<number>;
+  recipeId: string;
+  edit: boolean;
 }
 // const ratingChanged = ({newRating}: any) => {
 //   console.log(newRating)
 // }
 
 const calculateAverage = (rating: any) => {
-    const middleValue = rating.reduce((a: number, b: number) => a + b);
-    return middleValue / rating.length;
+  const middleValue = rating.reduce((a: number, b: number) => a + b);
+  return middleValue / rating.length;
 };
 
 const StyledStars = styled(ReactStars)`
-    display: flex;
-    justify-content: center;
-`
+  display: flex;
+`;
 
-const starColor = '#145858';
-const emptyStarColor = '#7faf7b'
+const starColor = "#145858";
+const emptyStarColor = "#7faf7b";
 
 const Stars = ({ recipeRatings, recipeId, edit }: ratingProps) => {
-
-    const ratingChanged = async (newRating: any) => {
-        console.log(newRating, recipeId)
-        await postRating(recipeId, newRating);
-    }
-    return (
-        <StyledStars
-            count={5}
-            value={calculateAverage(recipeRatings)}
-            color1={emptyStarColor}
-            color2={starColor}
-            size={40}
-            edit={edit}
-            half={false}
-            onChange={ratingChanged}
-
-        />
-    )
-}
-export default Stars
+  const ratingChanged = async (newRating: any) => {
+    console.log(newRating, recipeId);
+    await postRating(recipeId, newRating);
+  };
+  return (
+    <StyledStars
+      count={5}
+      value={calculateAverage(recipeRatings)}
+      color1={emptyStarColor}
+      color2={starColor}
+      size={40}
+      edit={edit}
+      half={false}
+      onChange={ratingChanged}
+    />
+  );
+};
+export default Stars;
